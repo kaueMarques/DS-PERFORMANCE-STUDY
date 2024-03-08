957f0bd72fc5b4bf45dabefa76eded5c5c289652
kauê Marques <mbkaue@gmail.com>
2024-03-08 17:51:21 -0300

    feat: fifo & lifo

--- LinkedList/python/linked_list.py
+++ LinkedList/python/linked_list.py
@@ -1,16 +1,47 @@
 #!/usr/bin/env python3
 from node import Node
 
-def add_element_at_end_recursion(root:Node, new_elment):
-    if root.next is not None:
-        add_element_at_end_recursion(root.next, new_elment)
+def add_element_at_end_recursion(node: Node, new_element):
+    if node.next is not None:
+        add_element_at_end_recursion(node.next, new_element)
     else:
-        root.next = Node(new_elment)
+        node.next = Node(new_element)
+
+def remove_last_node(node: Node):
+    if node.next is None or node.next.next is None:
+        node.next = None
+        return
+    remove_last_node(node.next)
+
+def get_last_node(root: Node):
+    return root.next.data
+
+def filo_operation(root:Node):
+    """FILO - First IN, Last OUT"""
+    data = get_last_node(root)
+    remove_last_node(root)
+    return data
+
+def fifo_operation(node:Node):
+    """FIFO - First In, First Out"""
+    data = node.next.data
+    node.next = node.next.next
+    return data
 
 if __name__ == '__main__':
-    root = Node(0)    
-
+    root = Node('Linked List')
     for element in range(10):
         add_element_at_end_recursion(root, element+1)
+
+    for i in range(3):
+        fifo_operation(root)
+
+    output = str(root)\
+        .replace('(','')\
+        .replace(')','')\
+        .replace("next =", '')\
+        .replace(",  None",'')\
+        .replace("content = Linked List,",'')\
+        + "\n"
     
-    print(root)
+    print(output)
--- LinkedList/python/node.py
+++ LinkedList/python/node.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 class Node:
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
         self.next = None
+        self.index = 0
+
         
     def __str__(self):
-        return f'<data: {self.data}, next: {self.next}>'
+        return f'\n(content = {self.data}, next = {self.next})'
